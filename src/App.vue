236b02b83806584484174cf1fa84cebd79047f31
Pedro0974 <pedro0702lucas@gmaill.com>
2023-05-19 10:29:53 -0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,15 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <hello-world />
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+import './assets/tailwind.css'
+import HelloWorld from './components/HelloWorld.vue';
+export default {
+  name: 'app',
+  components:{
+    HelloWorld,
   }
 }
-</style>
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,59 +1,150 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container-modelo-tabela">
+    <table class="container-tabela">
+      <thead class="cabecalho-tabela">
+        <tr>
+          <th scope="col" class="p-4">
+            <div class="flex items-center">
+              <input id="checkbox-all" type="checkbox"
+                class="checkbox">
+              <label for="checkbox-all" class="sr-only">checkbox</label>
+            </div>
+          </th>
+          <th scope="col" class="px-6 py-3">
+            #
+          </th>
+          <th scope="col" class="px-6 py-3">
+            Nome/Razão
+          </th>
+          <th scope="col" class="px-6 py-3">
+            Tipo
+          </th>
+          <th scope="col" class="px-6 py-3">
+            Status
+          </th>
+          <th scope="col" class="px-6 py-3">
+            Ações
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(pessoa, index) in listaPessoas"
+          :key="index"
+          class="escopo-tabela">
+          <td class="w-4 p-4">
+            <div class="flex items-center">
+              <input id="checkbox-table-1" type="checkbox"
+                class="checkbox">
+              <label for="checkbox-table-1" class="sr-only">checkbox</label>
+            </div>
+          </td>
+          <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+            {{ index + 1 }}
+          </th>
+          <td class="px-6 py-4">
+            {{ pessoa.nome_razao }}
+          </td>
+          <template v-if="pessoa.pessoa_fisica !== null">
+            <td class="px-6 py-4">
+              <i class="fas fa-user"></i>
+            </td>
+          </template>
+          <template v-if="pessoa.pessoa_juridica !== null">
+            <td class="px-6 py-4">
+              <i class="fas fa-factory"></i>
+            </td>
+          </template>
+          <template v-if="pessoa.pessoa_fisica !== null">
+            <tempalte v-if="pessoa.pessoa_fisica.ativo === true">
+              <td class="px-6 py-4">
+                <i class="fas fa-cicle-check"></i>
+              </td>
+            </tempalte>
+            <tempalte v-if="pessoa.pessoa_fisica.ativo === false">
+              <td class="px-6 py-4">
+                <i class="fas fa-circle-close"></i>
+              </td>
+            </tempalte>
+          </template>
+
+          <template v-if="pessoa.pessoa_juridica !== null">
+            <tempalte v-if="pessoa.pessoa_juridica.ativo === true">
+              <td class="px-6 py-4">
+                <i class="fas fa-cicle-check"></i>
+              </td>
+            </tempalte>
+            <tempalte v-if="pessoa.pessoa_juridica.ativo === false">
+              <td class="px-6 py-4">
+                <i class="fas fa-circle-close"></i>
+              </td>
+            </tempalte>
+          </template>
+
+          <td class="px-6 py-4">
+            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      error: [],
+      listaPessoas: [],
+    }
+  },
+
+  created() {
+    this.getListaPessoas()
+  },
+
+  methods: {
+    async getListaPessoas() {
+
+      const token = '9d66326f76faa71872b0ebec30f27e4e20539e7b'
+
+      axios.get('http://orolglass.managerglass:8000/api/v1/pessoas/', {
+        headers: {
+          'Authorization': `Token ${token}`
+        }
+      })
+        .then(response => {
+          this.listaPessoas = response.data
+          console.log(response.data)
+        })
+        .catch(error => {
+          console.log(error);
+        })
+
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+<style lang="scss">
+.container-modelo-tabela {
+  @apply relative overflow-x-auto shadow-md sm:rounded-lg;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.container-tabela{
+  @apply w-full text-sm text-left text-gray-500 dark:text-gray-400;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.cabecalho-tabela {
+  @apply text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400;
 }
-a {
-  color: #42b983;
+
+.checkbox {
+  @apply w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600;
+}
+
+.escopo-tabela {
+  @apply bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600;
 }
 </style>
